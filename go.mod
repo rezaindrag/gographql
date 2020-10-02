f08830975a746aa2dc5a4c568e88ef5322c02fd3
Reza Indra <dev.rezaindra@gmail.com>
2020-10-02 13:52:06 +0700

    feat: add test for resolver

--- go.mod
+++ go.mod
@@ -9,5 +9,6 @@ require (
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.6.0
+	github.com/stretchr/testify v1.4.0
 	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
 )
--- resolver/article_test.go
+++ resolver/article_test.go
@@ -1,1 +1,60 @@
 package resolver
+
+import (
+	"github.com/graphql-go/graphql"
+	"github.com/graphql-go/graphql/language/ast"
+	"github.com/pkg/errors"
+	"github.com/rezaindrag/gographql"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestArticleResolver_GetByID(t *testing.T) {
+	for name, test := range map[string]struct {
+		passParams graphql.ResolveParams
+		exptResult interface{}
+		exptError  gographql.ExtendedError
+	}{
+		"Successfully resolve getting article by ID": {
+			passParams: graphql.ResolveParams{
+				Args: map[string]interface{}{
+					"id": "2",
+				},
+				Info: graphql.ResolveInfo{
+					FieldASTs: []*ast.Field{
+						{
+							SelectionSet: ast.NewSelectionSet(&ast.SelectionSet{
+								Selections: []ast.Selection{
+									ast.NewField(&ast.Field{
+										Name: ast.NewName(&ast.Name{
+											Value: "id",
+										}),
+									}),
+									ast.NewField(&ast.Field{
+										Name: ast.NewName(&ast.Name{
+											Value: "title",
+										}),
+									}),
+								},
+							}),
+						},
+					},
+				},
+			},
+			exptResult: gographql.Article{
+				ID:    "2",
+				Title: "Foo",
+			},
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			articleResolver := NewArticleResolver().Build()
+			result, err := articleResolver.GetByID(test.passParams)
+			if err != nil {
+				assert.EqualError(t, errors.Cause(err), test.exptError.Err.Error())
+			}
+			assert.Equal(t, result, test.exptResult)
+		})
+	}
+
+}
